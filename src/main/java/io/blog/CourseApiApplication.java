6e5bf353cdde530b834b6e5b1b9281d67baeb15d
thiagohenriqueguimaraes <thiagohenriqueguimaraes@gmail.com>
2019-04-03 17:14:25 -0300

    File Upload

--- src/main/java/io/blog/CourseApiApplication.java
+++ src/main/java/io/blog/CourseApiApplication.java
@@ -1,14 +1,51 @@
 package io.blog;
-
+import io.blog.api.quote.Quote;
+import io.blog.storage.StorageProperties;
+import io.blog.storage.StorageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class CourseApiApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(CourseApiApplication.class);
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(CourseApiApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
+	}
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
+
 }
+
---
+++ src/main/java/io/blog/api/quote/QuoteController.java
@@ -0,0 +1,17 @@
+package io.blog.api.quote;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class QuoteController {
+    @GetMapping("/importa-json-quotas")
+    public void Import(){
+
+    }
+
+    @GetMapping("/busca-quotas")
+    public void BuscaQuotas(){
+
+    }
+}
